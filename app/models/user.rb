c7e2e7aaa28fcba9c44fa9245cefb6e88d9cb674
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2023-02-01 21:05:03 +0600

    Create DB

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_many :events
+
+  before_validation :downcase_email
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  private
+
+  def downcase_email
+    email&.downcase!
+  end
 end
