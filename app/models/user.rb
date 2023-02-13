9640ef0987f925f0df5846cbd0aa9ea641b96472
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2023-02-13 16:15:01 +0600

    Add migration for name to Users, full localization on russian

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,11 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
 
   before_validation :downcase_email
-  before_validation :set_name, on: :create
   validates :name, presence: true, length: { maximum: 35 }
 
   private
 
   def downcase_email
     email&.downcase!
-  end
-
-  def set_name
-    self.name = "User №#{rand(777)}" if self.name.blank?
   end
 end
