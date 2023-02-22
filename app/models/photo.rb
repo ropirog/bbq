8b7e9f1c52bf7f8209f9a32d384de3a795047807
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2023-02-22 20:52:14 +0600

    Edit photo functionality

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-
   has_one_attached :photo do |attachable|
     attachable.variant :common, resize_to_fit: [800, 800]
     attachable.variant :thumb, resize_to_fit: [100, 100]
@@ -10,6 +9,13 @@ class Photo < ApplicationRecord
 
   validates :event, presence: true
   validates :user, presence: true
+  validate :prohibition_to_add_photo, on: :create
 
   scope :persisted, -> { where "id IS NOT NULL" }
+
+  private
+
+  def prohibition_to_add_photo
+    errors.add(:user, I18n.t("errors.prohibition_to_add_photo")) unless event.subscribers.include?(user)
+  end
 end
