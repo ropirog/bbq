534a3170210c262ad9b5144269d4fe33d49e8cb4
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2023-02-26 21:10:33 +0600

    Add mail delivery about new subscriptions and new comments

--- app/mailers/message_mailer.rb
+++
@@ -1,7 +0,0 @@
-class MessageMailer < ApplicationMailer
-  def send_email(email:, subject:, body:)
-    @body = body
-
-    mail to: email, subject: subject
-  end
-end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,9 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.active_storage.service = :amazon
+
   config.action_mailer.delivery_method = :mailjet
   config.action_mailer.default_url_options = { :host => "bbq.myapp-s.space" }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
 end
