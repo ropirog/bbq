783aa83da516c8ce166a906fabb7464f644ebd55
i-eg54125@mail.ru <ropirog@LAPTOP-S2TQ7UDA.localdomain>
2023-03-05 13:16:25 +0600

    Few changes in photos controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -41,7 +41,7 @@ class PhotosController < ApplicationController
   end
 
   def notify_subscribers(photo)
-    all_emails = (@event.subscriptions.map(&:user_email) + [@event.user.email] - [photo.user.email]).uniq
+    all_emails = (photo.event.subscriptions.map(&:user_email) + [photo.event.user.email] - [photo.user.email]).uniq
     all_emails.each do |mail|
       EventMailer.photo(photo, mail).deliver_now
     end
